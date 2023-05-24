290e5d8dd9673eb99fb26dfbc6ba6ba27a3c67a0
Zaky97 <zaky7645@gmail.com>
2023-05-24 20:32:28 +0700

    new file:   index.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,132 @@
+function submitForm() {
+  // ambil nilai dari elemen input dan select
+  var name = document.getElementById("name").value;
+  var gender = document.querySelector('input[name="gender"]:checked').value;
+  var birthPlace = document.getElementById("birth-place").value;
+  var birthDate = document.getElementById("birth-date").value;
+  var education = document.getElementById("education").value;
+  var status = document.getElementById("status").value;
+  var hobby = document.getElementById("hobby").value;
+  var address = document.getElementById("address").value;
+  var photo = document.getElementById("photo").files[0];
+  var photoName = photo.name;
+
+  // tampilkan nilai pada halaman web
+  var output = document.getElementById("output");
+  var table = `
+  <style>
+  body {
+    background-color: #f4f4f4;
+  }
+
+  .BIO{
+    display: none;
+  }
+
+#output{
+  display: block;
+}
+
+  .container {
+    margin-top: 50px;
+  }
+
+  .card {
+    border: none;
+  }
+
+  .card-header {
+    background-color: #007bff;
+    color: #fff;
+  }
+
+  .card-title {
+    margin-bottom: 0;
+  }
+
+  table {
+    width: 100%;
+    margin-top: 20px;
+  }
+
+  table td,
+  table th {
+    padding: 10px;
+    border: 1px solid #ccc;
+  }
+
+  table th {
+    background-color: #f4f4f4;
+    text-align: left;
+  }
+
+  #photo-preview {
+    max-width: 200px;
+    max-height: 200px;
+    display: block;
+    margin: 0 auto;
+  }
+</style>
+</head>
+<body>
+<div class="container">
+  <div class="card">
+    <div class="card-header">
+      <h4 class="card-title mb-0">Profil</h4>
+    </div>
+    <div class="card-body">
+      <table>
+        <tbody>
+          <tr>
+            <td colspan="2"><img class="img-fluid" width="100%" height="100%" id="photo-preview" src="" alt=""></td>
+          </tr>
+          <tr>
+            <th scope="row">Nama</th>
+            <td>${name}</td>
+          </tr>
+          <tr>
+            <th scope="row">Jenis Kelamin</th>
+            <td>${gender}</td>
+          </tr>
+          <tr>
+            <th scope="row">Tempat Lahir</th>
+            <td>${birthPlace}</td>
+          </tr>
+          <tr>
+            <th scope="row">Tanggal Lahir</th>
+            <td>${birthDate}</td>
+          </tr>
+          <tr>
+            <th scope="row">Riwayat Pendidikan</th>
+            <td>${education}</td>
+          </tr>
+          <tr>
+            <th scope="row">Status</th>
+            <td>${status}</td>
+          </tr>
+          <tr>
+            <th scope="row">Hobi</th>
+            <td>${hobby}</td>
+          </tr>
+          <tr>
+            <th scope="row">Alamat</th>
+            <td>${address}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+  `;
+
+  output.innerHTML = table;
+
+  // tampilkan foto pada halaman web
+  var photoPreview = document.getElementById("photo-preview");
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    photoPreview.src = e.target.result;
+  };
+  reader.readAsDataURL(photo);
+  console.log(readAsDataURL.photo);
+}
